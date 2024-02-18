1e1c9be1b72946d95d75bf6f47f76a8833ffde42
gabigoranov <gabigoranov@abv.bg>
2024-02-18 21:02:45 +0200

    did sum stuff

---
+++ lib/soon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:market/models/user-service.dart';
+import 'package:market/models/user.dart';
+
+class Soon extends StatelessWidget {
+  const Soon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Coming Soon!"),
+      ),
+    );
+  }
+}
+
